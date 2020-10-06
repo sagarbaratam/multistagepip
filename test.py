a92f0aed17ff399e1e4cdd8fb6489ca494eef848
sagar baratam <sagarbabubaratam@gmail.com>
2020-10-06 06:09:16 +0000

    removed complex stuff

--- test.py
+++ test.py
@@ -4,20 +4,6 @@ import os
 import sys
 import yum
 
-yb = yum.YumBase()
-yb.setCacheDir()
-
-results = yb.pkgSack.returnNewestByNameArch(patterns=["python", “perl”])
-
-for pkg in results:
-    print "%s %s (%s) \n\t%s" % (pkg.name, pkg.version, pkg.arch, pkg.summary)
 
 
-plist = yb.rpmdb.returnPackages()
-l_plist = [p for p in plist if p.size > 1024 * 1024 * 10]
-print "Installed packages with size > 10MB:"
-for p in l_plist:
-    print "%s: %sMB" % (pkg, pkg.size / (1024 * 1024))
-
-
-
+os.system('yum repolist')
